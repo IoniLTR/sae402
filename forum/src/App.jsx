7b24e395f3a23da67427aecde4655a80eeab66c3
rootidle10 <clara.perotto@iut-dijon.u-bourgogne.fr>
2025-05-15 08:30:11 +0200

    Ajout header et ajout theme App.jsx

--- forum/src/App.jsx
+++ forum/src/App.jsx
@@ -1,14 +1,31 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./pages/Layout";
+import NoPage from "./pages/NoPage";
+import Home from "./pages/Home1";
+import AjouterForum from "./ajouter_forum"; 
 
-function App() {
-
-
+export default function App() {
+  const [inputname, setInputname] = useState('');
+  const [inputdescription, setInputdescription] = useState('');
+  const [inputtheme, setInputtheme] = useState('');
   return (
     <>
-      
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="ajouter" element={<AjouterForum />} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+    <AjouterForum inputname={inputname}
+        inputdescription={inputdescription}
+        inputtheme={inputtheme}
+        setInputname={setInputname}
+        setInputdescription={setInputdescription}
+        setInputtheme={setInputtheme}></AjouterForum>
     </>
-  )
+  );
 }
-
-export default App
