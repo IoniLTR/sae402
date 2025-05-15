ba7c3506bcf98532a0ad3f538d184ac584280713
IoniLTR <163018846+IoniLTR@users.noreply.github.com>
2025-05-15 11:46:28 +0200

    logout
    remplace app.jsx

--- forum/src/App.jsx
+++ forum/src/App.jsx
@@ -1,17 +1,17 @@
+// src/App.jsx
 import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Layout from "./pages/Layout";
 import NoPage from "./pages/NoPage";
-import VoirForum from "./pages/Home1"; 
+import VoirForum from "./pages/Home1";
 import FormulaireAjouterForum from "./pages/ajouter_formulaire";
 import FormulaireAjouterUtilisateur from "./pages/ajouterutilisateur";
 import ForumPage from './pages/ForumPage';
 import MessagePage from './pages/MessagePage';
 import LoginForm from './pages/LoginForm';
+import { AuthProvider } from "./context/AuthContext"; // 💡 Assure-toi que ce fichier existe
 
 export default function App() {
-
   const [inputname, setInputname] = useState('');
   const [inputdescription, setInputdescription] = useState('');
   const [inputtheme, setInputtheme] = useState('');
@@ -21,47 +21,44 @@ export default function App() {
   const [inputrole, setInputrole] = useState('');
 
   return (
-    <>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
             <Route path="/" element={<VoirForum />} />
-      <Route path="/forums/:id" element={<ForumPage />} />
-      <Route path="/messages/:id" element={<MessagePage />} />
-      <Route path="login" element={<LoginForm />} />
-          <Route 
-            path="Ajouterunforum" 
-            element={
-              <FormulaireAjouterForum
-                inputname={inputname}
-                inputdescription={inputdescription}
-                inputtheme={inputtheme}
-                setInputname={setInputname}
-                setInputdescription={setInputdescription}
-                setInputtheme={setInputtheme}
-              />
-            }
-          />
-          <Route 
-            path="Ajouterutilisateur" 
-            element={
-              <FormulaireAjouterUtilisateur
-                inputuser={inputuser}
-                inputpassword={inputpassword}
-                inputrole={inputrole}
-                setInputuser={setInputuser}
-                setInputpassword={setInputpassword}
-                setInputrole={setInputrole}
-              />
-            }
-          />
-
-         
-          <Route path="*" element={<NoPage />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-    </>
+            <Route path="/forums/:id" element={<ForumPage />} />
+            <Route path="/messages/:id" element={<MessagePage />} />
+            <Route path="login" element={<LoginForm />} />
+            <Route 
+              path="Ajouterunforum" 
+              element={
+                <FormulaireAjouterForum
+                  inputname={inputname}
+                  inputdescription={inputdescription}
+                  inputtheme={inputtheme}
+                  setInputname={setInputname}
+                  setInputdescription={setInputdescription}
+                  setInputtheme={setInputtheme}
+                />
+              }
+            />
+            <Route 
+              path="Ajouterutilisateur" 
+              element={
+                <FormulaireAjouterUtilisateur
+                  inputuser={inputuser}
+                  inputpassword={inputpassword}
+                  inputrole={inputrole}
+                  setInputuser={setInputuser}
+                  setInputpassword={setInputpassword}
+                  setInputrole={setInputrole}
+                />
+              }
+            />
+            <Route path="*" element={<NoPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
