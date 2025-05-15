64d241096aa0f9ca7b8dc5e802308cc9a5b1a305
rootidle10 <clara.perotto@iut-dijon.u-bourgogne.fr>
2025-05-15 08:31:09 +0200

    ajouter_forum component

---
+++ forum/src/ajouter_forum.jsx
@@ -0,0 +1,80 @@
+import React from "react";
+
+export default function AjouterForum({
+  inputname,
+  setInputname,
+  inputdescription,
+  setInputdescription,
+  inputtheme,
+  setInputtheme
+}) {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    fetch("http://rsantacruz.fr/backForum/api/forums/addForum", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name: inputname,
+        description: inputdescription,
+        theme: inputtheme,
+      }),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Erreur lors de l’ajout du forum");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log("Forum ajouté avec succès:", data);
+        setInputname("");
+        setInputdescription("");
+        setInputtheme("cinema");
+      })
+      .catch((error) => {
+        console.error("Erreur:", error);
+      });
+  };
+
+  return (
+    <div>
+      <h1>Ajouter un forum</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Nom :
+          <input
+            type="text"
+            value={inputname}
+            onChange={(e) => setInputname(e.target.value)} // Utilisation de setInputname
+          />
+        </label>
+        <br />
+        <label>
+          Description :
+          <input
+            type="text"
+            value={inputdescription}
+            onChange={(e) => setInputdescription(e.target.value)} // Utilisation de setInputdescription
+          />
+        </label>
+        <br />
+        <label htmlFor="theme">Thème :</label>
+        <select
+          id="theme"
+          value={inputtheme}
+          onChange={(e) => setInputtheme(e.target.value)} // Utilisation de setInputtheme
+        >
+          <option value="cinema">cinema</option>
+          <option value="sport">sport</option>
+          <option value="quatre">musique</option>
+        </select>
+        <br />
+        <button type="submit">Ajouter</button>
+      </form>
+    </div>
+  );
+}
