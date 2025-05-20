f016249d8a803ba37e3431d549dbee7c07dd8f77
rootidle10 <clara.perotto@iut-dijon.u-bourgogne.fr>
2025-05-20 11:20:48 +0200

    header_style

--- forum/src/pages/Layout.jsx
+++ forum/src/pages/Layout.jsx
@@ -6,18 +6,28 @@ import './layout.css';
 const Layout = () => {
   return (
     <>
-      <header>
-        <nav>
-          <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/Ajouterunforum">Ajouter un forum</Link></li>
-            <li><Link to="/Ajouterutilisateur">Ajouter un utilisateur</Link></li>
-            <li><Link to="/forums">forum</Link></li>
-            <li><Link to="/messages">message</Link></li>
-            <li><NavBar /></li> {/* Ici on affiche Login ou Bienvenue/Déconnexion */}
-          </ul>
-        </nav>
-      </header>
+    <header className="main-header">
+  <nav className="navbar">
+    <div className="navbar-content">
+      <div className="nav-left">
+        <li><Link to="/">Home</Link></li>
+      </div>
+
+      <div className="nav-right">
+        <input type="checkbox" id="menu-toggle" className="menu-toggle" />
+        <label htmlFor="menu-toggle" className="burger">&#9776;</label>
+
+        <ul className="nav-links">
+          <li><Link to="/Ajouterunforum">Ajouter un forum</Link></li>
+          <li><Link to="/Ajouterutilisateur">Ajouter un utilisateur</Link></li>
+          {/* <li><Link to="/forums">Forum</Link></li>
+          <li><Link to="/messages">Message</Link></li> */}
+          <li><NavBar /></li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</header>
       <main>
         <Outlet />
       </main>
